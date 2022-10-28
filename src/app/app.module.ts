6533d1377ea34413c238e7ab68a248eab1721c38
martijnvanderbruggen <martijnvanderbruggen@live.nl>
2022-10-28 15:21:06 +0200

    fixed merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CreateCodeSnippetComponent } from './create-code-snippet/create-code-snippet.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { TableComponent } from './table/table.component';
-import { DataTablesModule } from "angular-datatables";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CreateCodeSnippetComponent } from "./create-code-snippet/create-code-snippet.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { TableComponent } from "./table/table.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateCodeSnippetComponent,
-    TableComponent
-  ],
+  declarations: [AppComponent, CreateCodeSnippetComponent, TableComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    DataTablesModule
-    
-    
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,37 +1,28 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
-import { CodeSnippet } from '../code-snippet';
-import { map } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { CodeSnippet } from "../code-snippet";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  selector: "app-table",
+  templateUrl: "./table.component.html",
+  styleUrls: ["./table.component.scss"],
 })
 export class TableComponent implements OnDestroy, OnInit {
-
-  dtOptions: DataTables.Settings = {};
   CodeSnippets: CodeSnippet[] = [];
-  dtTrigger: Subject<any> = new Subject<any>();
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 2
-    };
-    this.httpClient.get<CodeSnippet[]>('http://localhost:8000/codeSnippets')
-      .subscribe(data => {
+    this.httpClient
+      .get<CodeSnippet[]>("http://localhost:8000/codeSnippets")
+      .subscribe((data) => {
         this.CodeSnippets = (data as any).data;
         // Calling the DT trigger to manually render the table
-        this.dtTrigger.next(void 0);
       });
   }
-  
+
   ngOnDestroy(): void {
     // Do not forget to unsubscribe the event
-    this.dtTrigger.unsubscribe();
   }
-
 }
